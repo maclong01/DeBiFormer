70d8f93649f8b45eb0a01a0117513312b3f2f823
Nguyen Huu Bao Long（グェンフウ　バオロン） <maclong01@gmail.com>
2024-09-29 03:20:39 +0900

    Update upernet_512_debi_base_160k.py

--- segmentation/configs/upernet_512_debi_base_160k.py
+++ segmentation/configs/upernet_512_debi_base_160k.py
@@ -183,11 +183,6 @@ lr_config = dict(
     min_lr=0.0,
     by_epoch=False)
 
-#lr_config = dict(policy='poly',
-#                 warmup='linear',
-#                 warmup_iters=1500,
-#                 warmup_ratio=1e-6,
-#                 power=1.0, min_lr=0.0, by_epoch=False)
 
 
 runner = dict(type='IterBasedRunner', max_iters=160000)
